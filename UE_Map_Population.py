5290a76051ff7bf491cf5f7495633dc8f9bbc52b
ianistor <ndrnistor@gmail.com>
2021-10-18 23:54:34 +0200

    fixed a lot of shit

--- UE_Map_Population.py
+++ UE_Map_Population.py
@@ -1,9 +1,10 @@
 import unreal
 import sys
+import os
 import qdarkstyle
-from PySide2 import QtGui
-from PySide2 import QtWidgets
+from PySide2 import QtGui,QtCore,QtWidgets
 from PySide2.QtGui import QPalette, QColor
+from PySide2.QtCore import Qt, QSize
 from unreal import StaticMesh as mesh
 from unreal import Vector
 
@@ -13,76 +14,121 @@ from unreal import Vector
 
 #TODO: Figure out how calculating bboxes works
 #TODO: Figure out how to move the next prop based on the previous prop Bbox
+#TODO: Expose options for display
+    #TODO: Text Labels, Row, and Columns
+    #TODO: Padding
 
 """
 APi docs
 https://docs.unrealengine.com/4.27/en-US/PythonAPI/search.html?q=bounding+box
 """
 
-# Global Variables 
+ 
 
 unreal.log("---Starting the show---")
 
-loop_count = 0
-coord_x = 0
-coord_y = 0
-coord_z = 250
+# Variables
+
+Author = "Ioan-Andrei Nistor"
+Contact = "ioan.andrei.nistor@gmail.com"
+StyleSheetFile = "D:/GitHubRepos/UnrealMapPopulator/UnrealMapPop/darkOrange.css"
 
 class TestWidget(QtWidgets.QWidget):
+    loop_count = 0
+    coord_x = 0
+    coord_y = 0
+    coord_z = 250
+
     def __init__(self, parent=None):
 
         super(TestWidget, self).__init__(parent)
+        # self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setWindowTitle("Level Populator")
+        self.setMinimumSize(200,100)
+        self.setWindowOpacity(0.9)
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        print (dir_path)
+
+        with open(StyleSheetFile, "r") as sh:
+            self.setStyleSheet(sh.read())
 
         vbox = QtWidgets.QVBoxLayout(self)
-        btn = QtWidgets.QPushButton('Test')
-        btn.clicked.connect(self.btn_clicked)
+        btn = QtWidgets.QPushButton('Populate Level')
+        btn.clicked.connect(self.btn_clicked)        
         vbox.addWidget(btn)
 
+        label_row = QtWidgets.QLabel()
+        label_row.setText(("Rows"))
+        vbox.addWidget(label_row)
+        row_spin = QtWidgets.QSpinBox()
+        row_spin.setMinimum(1)
+        vbox.addWidget(row_spin)
+
+        label_column = QtWidgets.QLabel()
+        label_column.setText(("Columns"))
+        vbox.addWidget(label_column)
+        column_spin = QtWidgets.QSpinBox()
+        column_spin.setMinimum(1)
+        vbox.addWidget(column_spin)
+        
+        btn_quit = QtWidgets.QPushButton('Quit')
+        btn_quit.clicked.connect(QtCore.QCoreApplication.instance().quit)
+        vbox.addWidget(btn_quit)
 
 
+
+
+    
+    def pop_message_window(self, title, description): # Self so it does not cry <3
+
+        unreal.EditorDialog.show_message(title, description,message_type=unreal.AppMsgType.OK)
         
     def populate_level(self):
 
-        assets = unreal.EditorUtilityLibrary.get_selected_assets() 
-
-        if len(assets) == 0:
+        # Still Need to Fix a proper offset between assets
+        self.assets = unreal.EditorUtilityLibrary.get_selected_assets() 
+        if len(self.assets) == 0:
             unreal.EditorDialog.show_message("Wold Populator", "Error: Please select Static Meshes from Content Browser!",message_type=unreal.AppMsgType.OK)
         else:      
-            for i in assets:
+            for i in self.assets:
                 asset_data = unreal.EditorUtilityLibrary.get_selected_asset_data()
-                asset_path = unreal.EditorUtilityLibrary.get_path_name(assets[loop_count])       # Getting the path to the selected asset
+                asset_path = unreal.EditorUtilityLibrary.get_path_name(self.assets[self.loop_count])       # Getting the path to the selected asset
                 try_to_load = unreal.EditorAssetLibrary.load_asset(asset_path)
                 unreal.log("---Calculating BBox Location Offsets---")
-                get_bbox_min = mesh.get_bounding_box(i).min.x
-        
-                get_bbox_max = mesh.get_bounding_box(i).max.x 
-
-                coord_x = coord_x - get_bbox_min *2.5
-                actor_location_math = unreal.Vector(coord_x, coord_y, coord_z)
-                print (actor_location_math)
+                self.get_bbox_min = mesh.get_bounding_box(i).min.x
+                self.get_bbox_max = mesh.get_bounding_box(i).max.x 
+                self.coord_x = self.coord_x - self.get_bbox_min *2.5
+                actor_location_math = unreal.Vector(self.coord_x, self.coord_y, self.coord_z)
+                print (actor_location_math.x, actor_location_math.y, actor_location_math.z)
                 actor_rotation = unreal.Rotator(0.0, 0.0, 0.0)      # Dont really think this will ever be needed but its here in case its needed
                 unreal.EditorLevelLibrary.spawn_actor_from_object(try_to_load, actor_location_math, actor_rotation)
-                iteration += 1 
-                # print ('Asset' , (asset_path) , 'placed at location ' , (actor_location))
-                # unreal.EditorDialog.show_message("Wold Populator", "Your assets have been placed in your level",message_type=unreal.AppMsgType.OK)
+                self.loop_count += 1 
+        return self.loop_count
+
+    def get_number_of_assets(self):
+        self.total_assets_places = str(str("Total of: ") + str(self.loop_count) + str(" assets placed"))
+        # self.pop_message_window("LevelPopulator", self.total_assets_places)
 
     def btn_clicked(self):
         print('Clicked')
-
         unreal.log('---Clicked---')
         self.populate_level()
+        self.get_number_of_assets()
+        self.loop_count = 0 # Reseting Counter in case you want to place it again
 
-    def do_something():
+    def quit_app(self):
+        app.quit()
 
-        # lets move stuff in functions
-        return
 
 
 unreal.log("---Curtain Drop---")
 
 app = None
 if not QtWidgets.QApplication.instance():
-	app = QtWidgets.QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
+else:
+    app = QtWidgets.QApplication.instance()
 
 
 # app.setStyleSheet(qdarkstyle.load_stylesheet_pyside2())
