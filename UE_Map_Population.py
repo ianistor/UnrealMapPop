f802440fd2e28aaa08db48e7e4cea2c8a63eb003
ianistor <ndrnistor@gmail.com>
2021-10-31 19:41:36 +0100

    started working on rows implementation fixed closing of the app made it header less created undo stack

--- UE_Map_Population.py
+++ UE_Map_Population.py
@@ -33,11 +33,19 @@ class TestWidget(QtWidgets.QWidget):
     coord_y = 0
     coord_x = 0
     coord_z = 250
+    current_bbox_x = 0
+    record_maxbbox_x = 0
+
+    #For Progress Bar if needed later
+    total_frames = 100
+    text_label_progress = "Placing your assets..."
+
+
 
     def __init__(self, parent=None):
 
         super(TestWidget, self).__init__(parent)
-        # self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setWindowFlag(Qt.FramelessWindowHint) # This makes its headerless (looks nicer)
         self.setWindowTitle("Level Populator")
         self.setMinimumSize(200,100)
         self.setWindowOpacity(0.9)
@@ -86,32 +94,52 @@ class TestWidget(QtWidgets.QWidget):
         print ("Looking into Class of Selected")
         #Returns class
 
+    def create_level_folder(self):
+        # Lets create a folder to place assets in
+        # unreal.EditorLevelUtils.get_name()
+        print (unreal.EditorLevelUtils.get_name())
+
 
     def populate_level(self):
 
         # Still Need to Fix a proper offset between assets
         self.assets = unreal.EditorUtilityLibrary.get_selected_assets() 
+        self.create_level_folder()
         if len(self.assets) == 0:
             unreal.EditorDialog.show_message("Wold Populator", "Error: Please select Static Meshes from Content Browser!",message_type=unreal.AppMsgType.OK)
         else:      
-            for i in self.assets:
-                asset_data = unreal.EditorUtilityLibrary.get_selected_asset_data()
-                # asset_type = unreal.EditorUtilityLibrary.get_class()
-                # print (asset_type)
-                asset_path = unreal.EditorUtilityLibrary.get_path_name(self.assets[self.loop_count])       # Getting the path to the selected asset
-                try_to_load = unreal.EditorAssetLibrary.load_asset(asset_path)
-              
-                unreal.log("---Calculating BBox Location Offsets---")
-                self.get_bbox_min = mesh.get_bounding_box(i).min.y
-                self.get_bbox_max = mesh.get_bounding_box(i).max.y 
-                self.coord_y = self.coord_y - self.get_bbox_min *2.5 + self.get_bbox_max
-                actor_location_math = unreal.Vector(self.coord_x, self.coord_y, self.coord_z)
-                print (actor_location_math.x, actor_location_math.y, actor_location_math.z)
-                actor_rotation = unreal.Rotator(0.0, 0.0, 0.0)      # Dont really think this will ever be needed but its here in case its needed
-                unreal.EditorLevelLibrary.spawn_actor_from_object(try_to_load, actor_location_math, actor_rotation)
-                # unreal.EditorLevelLibrary().spawn_actor_from_class(unreal.StaticMeshActor("TextRenderActor"), actor_location_math, actor_rotation)
-                self.loop_count += 1 
-                self.coord_y = self.coord_y - self.get_bbox_min *2.5
+            with unreal.ScopedEditorTransaction("Undo Stack") as trans:   #Undo Stack
+                # with unreal.ScopedSlowTask(self.total_frames, self.text_label_progress) as slow_task:
+                #     slow_task.make_dialog(True)
+                #     for i in range(self.total_frames):
+                #         if slow_task.should_cancel():
+                #             break
+                #         slow_task.enter_progress_frame(1)
+                        
+                for i in self.assets:
+                    asset_data = unreal.EditorUtilityLibrary.get_selected_asset_data()
+                    # asset_type = unreal.EditorUtilityLibrary.get_class()
+                    # print (asset_type)
+                    asset_path = unreal.EditorUtilityLibrary.get_path_name(self.assets[self.loop_count])       # Getting the path to the selected asset
+                    try_to_load = unreal.EditorAssetLibrary.load_asset(asset_path)
+                
+                    unreal.log("---Calculating BBox Location Offsets---")
+                    self.get_bbox_min = mesh.get_bounding_box(i).min.y
+                    self.get_bbox_max = mesh.get_bounding_box(i).max.y
+                    self.current_bbox_x = mesh.get_bounding_box(i).max.x
+                    if self.current_bbox_x > self.record_maxbbox_x:
+                        self.record_maxbbox_x = self.current_bbox_x
+                        print ("---Found bigger mesh, recording max size for offset---")
+                    self.coord_y = self.coord_y - self.get_bbox_min *2.5 + self.get_bbox_max # Not sure if i really need to + the bbox_max 
+                    actor_location_math = unreal.Vector(self.coord_x, self.coord_y, self.coord_z)
+                    print (actor_location_math.x, actor_location_math.y, actor_location_math.z)
+                    actor_rotation = unreal.Rotator(0.0, 0.0, 0.0)      # Dont really think this will ever be needed but its here in case its needed
+                    unreal.EditorLevelLibrary.spawn_actor_from_object(try_to_load, actor_location_math, actor_rotation)
+                    # unreal.EditorLevelLibrary().spawn_actor_from_class(unreal.StaticMeshActor("TextRenderActor"), actor_location_math, actor_rotation)
+                    self.loop_count += 1 
+                    self.coord_y = self.coord_y - self.get_bbox_min *2 # Keep offsetting
+                    
+                    # Get highest number of BBox on X from previous placement
                 
         return self.loop_count
 
@@ -128,7 +156,7 @@ class TestWidget(QtWidgets.QWidget):
 
     def quit_app(self):
         print("quiting")
-        app.close()
+        self.close()
 
 
 unreal.log("---Curtain Drop---")
