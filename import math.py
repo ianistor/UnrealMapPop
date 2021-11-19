f2be226c199c772ebaa211820a5aa57f1967e655
ianistor <ndrnistor@gmail.com>
2021-11-19 18:34:07 +0100

    submit tests

---
+++ import math.py
@@ -0,0 +1,13 @@
+import math
+ 
+# Printing the log base e of 14
+print ("Natural logarithm of 14 is : ", end="")
+print (math.log(100))
+ 
+# Printing the log base 5 of 14
+# print ("Logarithm base 5 of 14 is : ", end="")
+# print (math.log(14,5))
+
+
+a = math.sqrt(100)
+print (a)
